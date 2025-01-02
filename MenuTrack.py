7e3516c13c3252ed73babced31f2654b7f796232
furkansarsilmaz <160139624+furkansarsilmaz@users.noreply.github.com>
2025-01-02 18:47:12 +0300

    refactor UI

--- MenuTrack.py
+++ MenuTrack.py
@@ -7,27 +7,17 @@ import csv
 class App:
     def __init__(self,root):
         self.root = root
-        self.root.title("App")
-        self.root.geometry("300x300")
-        self.root.configure(background="lightblue")
+        self.ui = UI(self.root)
         self.menu = {
             "Kebab": 0 ,
             "Doner": 0,
             "Baklava":0,
             "Turkish coffee":0
         }
-        self.main_menu()
-
-    def main_menu(self):
-        self.clear_screen()
         self.buttonOrder = Button(self.root,text="Order",width=5,font=("arial",12,"italic"),command= self.getTable)
-        self.buttonOrder.grid(row=0,column=1,padx=110,pady=10)
-
         self.buttonExit = Button(self.root,text="Exit",width=5,font=("arial",12,"italic"),command= self.exit_App)
-        self.buttonExit.grid(row=1,column=1,padx=100,pady=10)
-
         self.buttonSave = Button(self.root,text="Save",width=5,font=("arial",12,"italic"),command= self.saveDay)
-        self.buttonSave.grid(row=2,column=1,padx=100,pady=10)
+        self.ui.main_menu(self.buttonOrder,self.buttonExit,self.buttonSave)
 
     def getTable(self):
         self.clear_screen()
--- UI.py
+++ UI.py
@@ -5,19 +5,8 @@ class UI:
         self.root.title("App")
         self.root.geometry("300x300")
         self.root.configure(background="lightblue")
-        self.menu = {
-            "Kebab": 0 ,
-            "Doner": 0,
-            "Baklava":0,
-            "Turkish coffee":0
-        }
-    def main_menu(self):
-        self.clear_screen()
-        self.buttonOrder = Button(self.root,text="Order",width=5,font=("arial",12,"italic"),command= self.getTable)
-        self.buttonOrder.grid(row=0,column=1,padx=110,pady=10)
 
-        self.buttonExit = Button(self.root,text="Exit",width=5,font=("arial",12,"italic"),command= self.exit_App)
-        self.buttonExit.grid(row=1,column=1,padx=100,pady=10)
-
-        self.buttonSave = Button(self.root,text="Save",width=5,font=("arial",12,"italic"),command= self.saveDay)
-        self.buttonSave.grid(row=2,column=1,padx=100,pady=10)
+    def main_menu(self,buttonOrder,buttonExit,buttonSave):
+        buttonOrder.grid(row=0,column=1,padx=110,pady=10)
+        buttonExit.grid(row=1,column=1,padx=100,pady=10)
+        buttonSave.grid(row=2,column=1,padx=100,pady=10)
